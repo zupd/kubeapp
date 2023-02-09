3ff0cfcc8e6897277fe6ac0e50b7f1d3927e09b8
student <student@kube>
2023-02-09 10:41:58 +0100

    fix

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello from my application!"
+    return "Hello from my application!\n"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8888)
