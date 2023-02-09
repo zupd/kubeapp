ca1d77b8106d049f0d788c58cd74b026dd68788b
student <student@kube>
2023-02-09 12:54:04 +0100

    add replicasets

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello from my application!\n"
+    return f"Hello from my application!\n{os.uname()}\n"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8888)
